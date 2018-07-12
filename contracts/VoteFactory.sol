54df17c6fb0212667cfe27f190db4bb442d38003
vladislav zhukov <vzhukov@protonmail.ch>
2018-07-12 14:40:58 +0500

    did added answer for third lesson and for previous  lessons

--- contracts/VoteFactory.sol
+++ contracts/VoteFactory.sol
@@ -1,31 +1,58 @@
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.24;
 
-contract VoteFactory {
+import "./Ownable.sol";
+
+contract VoteFactory is Ownable {
   
+    enum State {
+        Initial,
+        Started,
+        Stopped
+    }
+
     struct Vote {
+        State state;
         string question;
         string[] answers;
     } 
   
-    address owner;
     Vote[] public votes;
     mapping (uint256 => address) voteToOwner;
   
-    modifier onlyOwner(uint256 _voteId) {
+    modifier onlyVoteOwner(uint256 _voteId) {
         require(voteToOwner[_voteId] == msg.sender);
         _;
     }
-  
-    constructor() public {
-        owner = msg.sender;
+
+    modifier onlyInitialVote(uint256 _voteId) {
+        require(votes[_voteId].state == State.Initial);
+        _;
+    }
+
+    modifier onlyStartedVote(uint256 _voteId) {
+        require(votes[_voteId].state == State.Started);
+        _;
+    }
+
+    modifier onlyStoppedVote(uint256 _voteId) {
+        require(votes[_voteId].state == State.Stopped);
+        _;
     }
   
     function createVote(string _question) public {
-        uint256 id = votes.push(Vote(_question, new string[](0))) - 1;
+        uint256 id = votes.push(Vote(State.Initial, _question, new string[](0))) - 1;
         voteToOwner[id] = msg.sender;
     }
+
+    function startVote(uint256 _voteId) public onlyVoteOwner(_voteId) onlyInitialVote(_voteId) {
+        votes[_voteId].state = State.Started;
+    }
+
+    function stopVote(uint256 _voteId) public onlyVoteOwner(_voteId) onlyStartedVote(_voteId) {
+        votes[_voteId].state = State.Stopped;
+    }
   
-    function addAnswer(uint256 voteId, string _answer) public onlyOwner(voteId) {
-        votes[voteId].answers.push(_answer);
+    function addAnswer(uint256 _voteId, string _answer) public onlyVoteOwner(_voteId) onlyStartedVote(_voteId) {
+        votes[_voteId].answers.push(_answer);
     }
 }
--- test/test_VoteFactory.js
+++ test/test_VoteFactory.js
@@ -1,1 +1,22 @@
-This is a placeholder file to ensure the parent directory in the git repository. Feel free to remove.
+var VoteFactory = artifacts.require("./VoteFactory.sol");
+
+let CorrectQuestion = "New Question"
+let CorrectAnswer = "NewAnswer"
+
+contract('VoteFactory', async (accounts) => {
+
+  it("should create new vote"),  async () => {
+    let voteFactory = await VoteFactory.deployed();
+    await voteFactory.createVote(CorrectQuestion);
+    let createdVote = await voteFactory.votes[0].question;
+    assert.equal(createdVote, CorrectQuestion, "");
+  }
+
+  it("should add new answer to vote"),  async () => {
+    let voteFactory = await VoteFactory.deployed();
+    await voteFactory.createVote(CorrectQuestion);
+    await voteFactory.addAnswer(0, CorrectAnswer);
+    let createdAnswer = await voteFactory.votes[0].answers[0];
+    assert.equal(createdAnswer, CorrectAnswer, "");
+  }
+});
