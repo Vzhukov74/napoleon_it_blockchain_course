7366c32190b1d1c2832aef92090e7204cb6579de
vladislav zhukov <vzhukov@protonmail.ch>
2018-07-13 13:12:58 +0500

    did added gitignore and updated test

--- test/test_VoteFactory.js
+++ test/test_VoteFactory.js
@@ -1,22 +1,48 @@
+//require('babel-register');
+//require('babel-polyfill');
+
+//const BigNumber = web3.BigNumber;
+//var ethUtil = require('ethereumjs-util')
+//var Tx = require('ethereumjs-tx');
+//const expect = require('chai').expect;
+//const should = require('chai')
+//    .use(require('chai-as-promised'))
+//    .use(require('chai-bignumber')(web3.BigNumber))
+//    .should();
+
+import expectThrow from './helpers/expectThrow';
+
 var VoteFactory = artifacts.require("./VoteFactory.sol");
 
-let CorrectQuestion = "New Question"
-let CorrectAnswer = "NewAnswer"
+contract('VoteFactory', function(accounts) {
+  var voteFactory;
 
-contract('VoteFactory', async (accounts) => {
+  const owner = accounts[0];
+  const creator = accounts[1];
+  const user = accounts[2];
 
-  it("should create new vote"),  async () => {
-    let voteFactory = await VoteFactory.deployed();
-    await voteFactory.createVote(CorrectQuestion);
+  const CorrectQuestion = "New Question"
+  const CorrectAnswer = "NewAnswer"
+
+  beforeEach('setup contract for each test', async function() {
+    voteFactory = await VoteFactory.new({from: owner});
+  });
+
+  it("should create new vote"),  async function() {
+    await voteFactory.createVote(CorrectQuestion, {from: creator});
     let createdVote = await voteFactory.votes[0].question;
     assert.equal(createdVote, CorrectQuestion, "");
   }
 
-  it("should add new answer to vote"),  async () => {
-    let voteFactory = await VoteFactory.deployed();
-    await voteFactory.createVote(CorrectQuestion);
-    await voteFactory.addAnswer(0, CorrectAnswer);
+  it("should add new answer to vote"),  async function() {
+    await voteFactory.createVote(CorrectQuestion, {from: creator});
+    await voteFactory.addAnswer(0, CorrectAnswer, {from: creator});
     let createdAnswer = await voteFactory.votes[0].answers[0];
     assert.equal(createdAnswer, CorrectAnswer, "");
+  }
+
+  it("should add new answer to a stranger vote"),  async function() {
+    await voteFactory.createVote(CorrectQuestion, {from: creator});
+    await expectThrow(voteFactory.addAnswer(0, answer0, {from: user}));
   }
 });
